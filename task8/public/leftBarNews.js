a1ec4f4381750cef1cefc2391b5a494af7fd71ba
Vladislav Zinchuk <zima04zima@gmail.com>
2017-04-04 23:13:32 +0300

    task8

---
+++ task8/public/leftBarNews.js
@@ -0,0 +1,55 @@
+function getInformation(){
+    var tem = JSON.parse(dbModel.getArrayOfArticals());
+    if(tem) {
+        articleModel.stringToDate(tem);
+        articleModel.reWrite(tem);
+    }
+    var newArticals = articleModel.getArticles(0,5);
+    var adress = "Item";
+    var stringId ="get-id";
+    for(var j = 0 ; j < 5; j++) {
+        adress = "Item" + j;
+        document.getElementById(stringId+j).setAttribute(stringId + j,newArticals[j].id)
+        document.getElementsByClassName(adress)[0].childNodes[1].textContent = newArticals[j].createdAt.toLocaleDateString("ru", options1);
+        var newTags = "";
+        for (var i = 0; i < newArticals[j].tags.length; i++) {
+            newTags += "#" + newArticals[j].tags[i] + " ";
+        }
+        document.getElementsByClassName(adress)[0].childNodes[3].textContent = newTags;
+        document.getElementsByClassName(adress)[0].childNodes[6].textContent = newArticals[j].title;
+    }
+}
+var options1 = {
+    hour: 'numeric',
+    minute: 'numeric',
+}
+
+function lookItemFromBar(el) {
+    var article = articleModel.getArticle(el.id);
+    document.querySelector('.main-page').style.display = 'none';
+    document.querySelector('.full-news').style.display = 'block';
+    document.querySelector('.article-item-title').textContent = article.title;
+    document.querySelector('.article-title-img').setAttribute('src', article.img);
+    document.querySelector('.article-item-content').textContent = article.content;
+    document.querySelector('.article-item-author').textContent = article.author;
+    document.querySelector('.article-item-date').textContent = article.createdAt.toLocaleDateString("ru", options);
+    if(article.tags[0])
+        document.querySelector('#article-tags-first').textContent = "#" + article.tags[0];
+    if(article.tags[1])
+        document.querySelector('#article-tags-second').textContent = "#" + article.tags[1];
+    if(article.tags[2])
+        document.querySelector('#article-tags-third').textContent = "#" + article.tags[2];
+    if(article.tags[3])
+        document.querySelector('#article-tags-fourth').textContent = "#" + article.tags[3];
+    if(article.tags[4])
+        document.querySelector('#article-tags-fifth').textContent = "#" + article.tags[4];
+}
+
+var options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+};
---
+++ task8/public/slideshow.js
@@ -0,0 +1,52 @@
+var slide = 1;
+
+function getNameItem(num) {
+    var items = articleModel.getArticles(0,6);
+    return items[num].title;
+}
+function getNameImage(num){
+    var items = articleModel.getArticles(0,6);
+    return items[num].img;
+}
+
+function getIdItem(num) {
+    var items = articleModel.getArticles(0,6);
+    return items[num].id;
+}
+
+function changeSlide() {
+    var tem = JSON.parse(dbModel.getArrayOfArticals());
+
+    if(tem) {
+        articleModel.stringToDate(tem);
+        articleModel.reWrite(tem);
+    }
+    if (slide == 1) {
+        document.getElementById("first-slide").src =  getNameImage(0);
+        document.getElementById("slide-text-first").innerHTML = getNameItem(0);
+        document.getElementById("slideItem1").setAttribute("slideItem1",getIdItem(0));
+        document.getElementById("second-slide").src =  getNameImage(1);
+        document.getElementById("slide-text-second").innerHTML = getNameItem(1);
+        document.getElementById("slideItem2").setAttribute("slideItem2",getIdItem(1));
+        document.getElementById("third-slide").src =  getNameImage(2);
+        document.getElementById("slide-text-third").innerHTML = getNameItem(2);
+        document.getElementById("slideItem3").setAttribute("slideItem3",getIdItem(2));
+
+    }
+    if (slide == 2) {
+        document.getElementById("first-slide").src = getNameImage(3);
+        document.getElementById("slide-text-first").innerHTML = getNameItem(3);
+        document.getElementById("slideItem1").setAttribute("slideItem3",getIdItem(3));
+        document.getElementById("second-slide").src = getNameImage(4);
+        document.getElementById("slide-text-second").innerHTML = getNameItem(4);
+        document.getElementById("slideItem2").setAttribute("slideItem2",getIdItem(4));
+        document.getElementById("third-slide").src = getNameImage(5);
+        document.getElementById("slide-text-third").innerHTML = getNameItem(5);
+        document.getElementById("slideItem3").setAttribute("slideItem3",getIdItem(5));
+    }
+    slide++;
+    if (slide > 2) {
+        slide = 1;
+    }
+    setTimeout(changeSlide, 3000);
+}
